4d9160111bfe82868fa9b059bec1cadfed0524e2
xriis <xriis@x7i>
2024-04-04 23:31:58 -0300

    Punto 2 realizado nuevamente

--- TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,8 +3,14 @@ package ar.edu.unju.fi.ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String country_name;
+		int age;
+		float build_high;
+		float product_price;
+		long phone_number;
+		double cos = Math.cos(0.5);
+		
 	}
 
 }
