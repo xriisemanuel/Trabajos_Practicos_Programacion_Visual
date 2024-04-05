347c8a7b1df75111e9d507f20d7662e21b6eedf2
xriis <xriis@x7i>
2024-04-04 23:27:02 -0300

    Punto 1 nuevamente realizado y terminado

--- TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,13 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		float n1 = 8f, n2 = 5f, n3 = 1f, n4 = 20f, n5 = 9f, promedio = 0;
+		
+		promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+		
+		System.out.println("El promedio es: " + promedio);
+		
 	}
 
 }
