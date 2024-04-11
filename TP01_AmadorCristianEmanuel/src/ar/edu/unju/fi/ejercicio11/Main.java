319e9df50b2090f85f0159b40ad0fba5677f45ec
xriis <xriis@x7i>
2024-04-11 12:10:45 -0300

    Continuación del tp1 - Punto 11 finalizado

---
+++ TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j = 40;
+		for (int i = 2; j > 0; i++) {
+			System.out.print(i*j + " ");
+			j--;
+		}
+	}
+
+}
