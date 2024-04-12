751c259be7bbe60ec7af96428b4e9df45a96dc79
xriis <xriis@x7i>
2024-04-11 23:17:57 -0300

    Ejercicio N12 de la segunda parte del TP1 Finalizada +- correcciones

--- TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio12/main/Main.java
+++ TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio12/main/Main.java
@@ -1,10 +1,60 @@
 package ar.edu.unju.fi.ejercicio12.main;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio12.model.Persona;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Persona unaPersona = new Persona();
+		
+		DateFormat formateador = new SimpleDateFormat("dd/M/yy");
+		
+		System.out.print("Ingrese su nombre: ");
+		unaPersona.setName(sc.next());
+		
+		System.out.print("Ingrese una fecha con el estilo: \"dd/M/yy\": ");
+		Date fecha = formateador.parse(sc.next());
+		
+		Calendar calendario = new GregorianCalendar();
+		
+		calendario.setTime(fecha);
+		unaPersona.setDate(calendario);
+		sc.close();
+		System.out.println(unaPersona.toString()+ "\nSigno Zodiacal: "+unaPersona.zodiacSign()+ "\nEstacion: "+ unaPersona.yearStation(calendario));
+		//System.out.println(unaPersona.toS+ "\nSigno Zodiacal: "+unaPersona.zodiacSign(unaPersona.getDate())+ "\nEstacion: "+ unaPersona.yearStation(unaPersona.getDate())tring()+"\n Edad: "+unaPersona.years(unaPersona.getDate().get(Calendar.YEAR)));
+		/*
+		DateFormat formateador = new SimpleDateFormat("dd/M/yy");
+		
+		Date fecha = formateador.parse("11/4/2024");
+		
+		Calendar calendario = new GregorianCalendar();
+		
+		calendario.setTime(fecha);
+		
+		unaPersona.setName("XriisEmanuel");
+		unaPersona.setDate(calendario);
+		
+		
+		
+		System.out.println("año Nacimiento: "+unaPersona.getDate().get(Calendar.YEAR));
+		System.out.println("mes Nacimiento: "+(1 + unaPersona.getDate().get(Calendar.MONTH)));
+		System.out.println("dia Nacimiento: "+unaPersona.getDate().get(Calendar.DAY_OF_MONTH));
+		
+		int year = unaPersona.getDate().get(Calendar.YEAR);
+		
+		unaPersona.getDate().compareTo(calendario);*/
+		
 	}
 
 }
--- TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
+++ TP01_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
@@ -1,5 +1,154 @@
 package ar.edu.unju.fi.ejercicio12.model;
 
-public class Persona {
+import java.util.Calendar;
 
+public class Persona {
+	private String name;
+	private Calendar date;
+	
+	public Persona() {};
+	
+	public Persona(String name, Calendar date) {
+		super();
+		this.name = name;
+		this.date = date;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Calendar getDate() {
+		return date;
+	}
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+	
+	public int years(int year) {
+		
+		Calendar hoy = Calendar.getInstance();
+		return hoy.get(Calendar.YEAR) - year;
+		
+	}
+	
+	public String zodiacSign() {
+		
+		Calendar fecha_inicio = Calendar.getInstance();
+		Calendar fecha_fin = Calendar.getInstance();
+		
+		fecha_inicio.set(date.get(Calendar.YEAR),2,21);
+		fecha_fin.set(date.get(Calendar.YEAR),3,19);
+		if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+			return "Aries";
+		} else {
+			fecha_inicio.set(date.get(Calendar.YEAR),3,20);
+			fecha_fin.set(date.get(Calendar.YEAR),4,20);
+			if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+				return "Tauro";
+			} else {
+				fecha_inicio.set(date.get(Calendar.YEAR),4,21);
+				fecha_fin.set(date.get(Calendar.YEAR),5,20);
+				if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+					return "Geminis";
+				} else {
+					fecha_inicio.set(date.get(Calendar.YEAR),5,21);
+					fecha_fin.set(date.get(Calendar.YEAR),6,22);
+					if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+						return "Cancer";
+					} else {
+						fecha_inicio.set(date.get(Calendar.YEAR),6,23);
+						fecha_fin.set(date.get(Calendar.YEAR),7,22);
+						if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+							return "Leo";
+						} else {
+							fecha_inicio.set(date.get(Calendar.YEAR),6,23);
+							fecha_fin.set(date.get(Calendar.YEAR),7,22);
+							if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+								return "Virgo";
+							} else {
+								fecha_inicio.set(date.get(Calendar.YEAR),7,23);
+								fecha_fin.set(date.get(Calendar.YEAR),9,22);
+								if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+									return "Libra";
+								} else {
+									fecha_inicio.set(date.get(Calendar.YEAR),9,23);
+									fecha_fin.set(date.get(Calendar.YEAR),10,21);
+									if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+										return "Escorpio";
+									} else {
+										fecha_inicio.set(date.get(Calendar.YEAR),10,22);
+										fecha_fin.set(date.get(Calendar.YEAR),11,21);
+										if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+											return "Sagitario";
+										} else {
+											fecha_inicio.set(date.get(Calendar.YEAR),11,22);
+											fecha_fin.set(date.get(Calendar.YEAR),0,19);
+											if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+												return "Capricornio";
+											} else {
+												fecha_inicio.set(date.get(Calendar.YEAR),0,20);
+												fecha_fin.set(date.get(Calendar.YEAR),1,19);
+												if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+													return "Acuario";
+												} else {
+													fecha_inicio.set(date.get(Calendar.YEAR),1,20);
+													fecha_fin.set(date.get(Calendar.YEAR),2,20);
+													if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+														return "Piscis";
+													} else {
+														return "Error de signo Zodiacal";
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public String yearStation(Calendar date) {
+		
+		Calendar fecha_inicio = Calendar.getInstance();
+		Calendar fecha_fin = Calendar.getInstance();
+		
+		fecha_inicio.set(date.get(Calendar.YEAR),2,21);
+		fecha_fin.set(date.get(Calendar.YEAR),5,20);
+		if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+			return "Otonio";
+		} else {
+			fecha_inicio.set(date.get(Calendar.YEAR),5,21);
+			fecha_fin.set(date.get(Calendar.YEAR),8,20);
+			if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+				return "Invierno";
+			} else {
+				fecha_inicio.set(date.get(Calendar.YEAR),8,21);
+				fecha_fin.set(date.get(Calendar.YEAR),11,20);
+				if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+					return "Primavera";
+				} else {
+					fecha_inicio.set(date.get(Calendar.YEAR),11,21);
+					fecha_fin.set(date.get(Calendar.YEAR),2,20);
+					if ((date.after(fecha_inicio)) && (date.before(fecha_fin))) {
+						return "Verano";
+					} else {
+						return "Error al elegir estacion";
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "\nDatos de: " + name + "\nFecha nacimiento: " + date.get(Calendar.DAY_OF_MONTH)+"/"+date.get(Calendar.MONTH) +"/"+date.get(Calendar.YEAR);
+	}
+	
+	
 }
